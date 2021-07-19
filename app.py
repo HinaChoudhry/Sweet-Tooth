6bf26009be54c0787bedf4aa7f2776dd56459935
HinaChoudhry <hinachoudhry@msn.com>
2021-07-19 09:55:49 +0000

    Created a 404 template and added python functionality for 404

--- app.py
+++ app.py
@@ -256,6 +256,11 @@ def forbidden(error):
     return render_template('403.html'), 403
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
